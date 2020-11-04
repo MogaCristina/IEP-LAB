18830bf5a2cfa909a4073bc7d32d6387bb883e94
Moga Cristina <mogacriss@yahoo.com>
2020-11-04 15:13:53 +0200

    cmake build

---
+++ Iep1/build/CMakeFiles/food.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/food.dir/src/Meniu.cpp.o"
+  "CMakeFiles/food.dir/src/Necopiabil.cpp.o"
+  "CMakeFiles/food.dir/src/Restaurant.cpp.o"
+  "CMakeFiles/food.dir/src/main.cpp.o"
+  "food"
+  "food.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/food.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
